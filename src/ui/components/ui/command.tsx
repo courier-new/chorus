3d30d8590d6e16ce268c4773bbc39729a872982a
Kelli Rockwell <kellirockwell@gmail.com>
2025-12-23 17:39:56 -0800

    feat: Match command item selected style to model badge (#5)

--- src/ui/components/ui/command.tsx
+++ src/ui/components/ui/command.tsx
@@ -172,7 +172,7 @@ const CommandItem = React.forwardRef<
     <CommandPrimitive.Item
         ref={ref}
         className={cn(
-            "relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 outline-none weight-medium data-[disabled=true]:pointer-events-none data-[selected='true']:bg-sidebar-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+            "relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 outline-none weight-medium data-[disabled=true]:pointer-events-none data-[selected=true]:bg-secondary border data-[selected=false]:!border-background data-[selected=true]:!border-transparent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
             className,
         )}
         {...props}
