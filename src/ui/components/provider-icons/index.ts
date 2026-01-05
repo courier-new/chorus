ea3241aef7e8e1f951be04b75410fdca0c17d3d4
Kelli Rockwell <kelli.rockwell@growtherapy.com>
2026-01-04 23:52:14 -0800

    Replace provider SVG images with components

---
+++ src/ui/components/provider-icons/index.ts
@@ -0,0 +1,25 @@
+// AI Company Logo Icons as React SVG Components
+
+export { AI21Icon } from "./AI21Icon";
+export { AionLabsIcon } from "./AionLabsIcon";
+export { ArceeIcon } from "./ArceeIcon";
+export { ByteDanceIcon } from "./ByteDanceIcon";
+export { CohereIcon } from "./CohereIcon";
+export { DeepCogitoIcon } from "./DeepCogitoIcon";
+export { DeepSeekIcon } from "./DeepSeekIcon";
+export { IBMIcon } from "./IBMIcon";
+export { InceptionIcon } from "./InceptionIcon";
+export { InflectionIcon } from "./InflectionIcon";
+export { KimiIcon } from "./KimiIcon";
+export { LiquidIcon } from "./LiquidIcon";
+export { MinimaxIcon } from "./MinimaxIcon";
+export { MistralIcon } from "./MistralIcon";
+export { MorphIcon } from "./MorphIcon";
+export { NousResearchIcon } from "./NousResearchIcon";
+export { NvidiaIcon } from "./NvidiaIcon";
+export { OpenRouterIcon } from "./OpenRouterIcon";
+export { QwenIcon } from "./QwenIcon";
+export { RelaceIcon } from "./RelaceIcon";
+export { XaiIcon } from "./XaiIcon";
+export { ZaiIcon } from "./ZaiIcon";
+
